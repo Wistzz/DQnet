7704930fac5e1ba84f8e9add15a04c5c72371527
CVPR23 <101049461+CVPR23@users.noreply.github.com>
2022-11-12 19:00:28 +0800

    Update module_ops.py

--- utils/ops/module_ops.py
+++ utils/ops/module_ops.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2021/5/17
-# @Author  : Lart Pang
-# @GitHub  : https://github.com/lartpang
-from typing import Callable
 
+from typing import Callable
 import torch
 
 
