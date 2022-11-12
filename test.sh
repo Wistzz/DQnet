05df3653beeb8e032a8aa3fd0368510a20eb0d43
CVPR23 <101049461+CVPR23@users.noreply.github.com>
2022-11-12 19:11:54 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -11,12 +11,12 @@ echo 'For COD'
 python test.py --config ./configs/DQnet/DQnet.py \
     --model-name DQnet \
     --batch-size 22 \
-    --load-from ./output/ForSharing/cod_zoomnet_r50_bs8_e40_2022-03-04.pth \
-    --save-path ./output/ForSharing/COD_Results
+    --load-from  \
+    --save-path 
 
 echo 'For SOD'
 python test.py --config ./configs/DQnet/DQnet.py \
     --model-name DQnet \
     --batch-size 22 \
-    --load-from ./output/ForSharing/sod_zoomnet_r50_bs22_e50_2022-03-04_fixed.pth \
-    --save-path ./output/ForSharing/SOD_Results
+    --load-from  \
+    --save-path 
