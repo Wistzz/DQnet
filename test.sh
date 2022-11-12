9f1f4332856db06da2b4988a33076468e48d9cbc
CVPR23 <101049461+CVPR23@users.noreply.github.com>
2022-11-12 19:10:57 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-set -e          # 只要脚本发生错误就停止运行
-set -u          # 如果遇到不存在的变量就报错并停止执行
-set -x          # 运行指令结果的时候，输出对应的指令
-set -o pipefail # 确保只要一个子命令失败，整个管道命令就失败
+set -e         
+set -u      
+set -x          
+set -o pipefail 
 
 export CUDA_VISIBLE_DEVICES="$1"
 echo 'Excute the script on GPU: ' "$1"
