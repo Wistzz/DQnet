66b67c774f53cbc849b241cff74b8056432eff80
CVPR23 <101049461+CVPR23@users.noreply.github.com>
2023-04-25 17:25:01 +0800

    Delete methods directory

--- methods/DQnet/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2021/7/24
-# @Author  : Lart Pang
-# @GitHub  : https://github.com/lartpang
--- methods/__init__.py
+++
@@ -1,3 +0,0 @@
-
-# from .DQnet.zoomnet_original import original_ZoomNet, original_ZoomNet_CK
-from .DQnet.DQnet import DQnet
